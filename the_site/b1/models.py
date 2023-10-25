80ef0673da9de9e4d2819c8a382f507506e88622
spacecreator8 <spcc888@ya.ru>
2023-10-25 10:52:40 +0700

    Сделал БД

--- the_site/b1/models.py
+++ the_site/b1/models.py
@@ -1,2 +1,58 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
 
+
+class User(AbstractUser):
+    name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
+    surname = models.CharField(max_length=254, verbose_name='Фамилия', blank=False)
+    patronymic = models.CharField(max_length=254, verbose_name='Отчество', blank=True)
+    username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
+    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
+    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+    agreement = models.BooleanField(verbose_name='Согласие на обработку персоональных данных', blank=False)
+    applications = models.ForeignKey('Application', on_delete=models.CASCADE, verbose_name='Заявки', null=True,
+                                     related_name='user')
+
+    USERNAME_FIELD = 'username'
+
+    def full_name(self):
+        return self.surname + ' ' + self.name + ' ' + self.patronymic
+
+    def __str__(self):
+        self.full_name()
+
+
+def get_name_file(instance, filename):
+    return '/'.join([get_random_string(length=5) + '_' + filename])
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Имя заявки', blank=False)
+    description = models.CharField(max_length=254, verbose_name='Описание', blank=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория желаемого стиля', blank=False)
+    photo = models.ImageField(max_length=254, verbose_name='Проект помещения',upload_to=get_name_file, blank=True, validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
+
+    STATUS_CHOISE = [
+        ('Accepted', 'Принято в работу'),
+        ('Finished', 'Выполнено')
+    ]
+
+    status = models.CharField(max_length=254, verbose_name='Статус', choices=STATUS_CHOISE, default='Accepted')
+    suggestions = models.ForeignKey('Realization', on_delete=models.CASCADE, verbose_name='Предложения реализации', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Название категории', blank=False)
+
+    def __str__(self):
+        self.name
+
+
+class Realization(models.Model):
+    description = models.CharField(max_length=254, verbose_name='Описание', blank=False)
+    image =  models.ImageField(max_length=254, upload_to=get_name_file, blank=True, validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
--- the_site/the_site/settings.py
+++ the_site/the_site/settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'main.AdvUser'
+AUTH_USER_MODEL = 'b1.User'
